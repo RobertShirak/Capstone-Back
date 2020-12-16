20ecb954cf04f6988029656dfe9c2eb378f9ae04
Robert Shirak <bcshirak1@gmail.com>
2020-12-16 11:40:41 -0500

    yerrrrrr

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(cors())
 app.set("json spaces", 4);
 
-app.get('/', (req, res));
+app.get('/');
 
 app.use("/desserts", require("./routes/dessertsRoutes"));
 app.use("/dinner", require("./routes/dinnerRoutes"));
